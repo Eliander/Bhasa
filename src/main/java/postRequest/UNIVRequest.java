62adcefef5ec1ec20013ab4115bee5a48a82f125
Eliander <Eliander@192.168.1.12>
2017-11-12 00:21:42 +0100

    Completed parser JSON courses values to java objects

--- src/main/java/postRequest/UNIVRequest.java
+++ src/main/java/postRequest/UNIVRequest.java
@@ -10,8 +10,6 @@ import java.io.DataOutputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.util.Date;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.net.ssl.HttpsURLConnection;
 import org.apache.logging.log4j.LogManager;
 
