38d8872834a3d3a9c1b445426a0538912874159a
Elia <Elia@192.168.1.9>
2018-03-02 09:03:36 +0100

    Minor implements at imageCreator
    change font
    add time

--- src/main/java/images/ImageCreator.java
+++ src/main/java/images/ImageCreator.java
@@ -12,6 +12,7 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import javax.imageio.ImageIO;
+import persistence.Courses;
 import persistence.Timetable;
 import utilities.Utilities;
 
@@ -27,14 +28,13 @@ public class ImageCreator {
     public void drawImage(Timetable timetable) throws Exception {
         try {
             int width = 500;
-            int widthForTime = 500;
             int offsetForTime = 10;
             int base = heightForElement;
 
             BufferedImage bi = new BufferedImage(width, 1045, BufferedImage.TYPE_INT_ARGB);
 
             Graphics2D g2d = bi.createGraphics();
-            g2d.setFont(new Font("TimesRoman", Font.PLAIN, 12));
+            g2d.setFont(new Font("Arial", Font.PLAIN, 10));
 
             boolean isHalf = false;
             int hour = 8;
@@ -51,7 +51,7 @@ public class ImageCreator {
                     message += ".00";
                 }
                 g2d.setColor(Color.WHITE);
-                g2d.fillRect(0, base, widthForTime, heightForElement);
+                g2d.fillRect(0, base, width, heightForElement);
                 g2d.setPaint(Color.black);
                 g2d.drawString(message, offsetForTime, base + 30);
                 base += heightForElement;
@@ -73,10 +73,6 @@ public class ImageCreator {
         }
     }
 
-    private int calcHeight(int elements) {
-        return heightForElement * (elements + 1);
-    }
-
     private int holeNumber(String string) {
         String[] split = string.split(":");
         int value = Integer.parseInt(split[0]);
@@ -94,14 +90,15 @@ public class ImageCreator {
             
             int base = holeNumber(timetable.getCourses().get(i).getStart())*heightForElement;
             int end = holeNumber(timetable.getCourses().get(i).getEnd())*heightForElement;
-            
             g2d.setColor(utility.randHEXColor());
             String message = timetable.getCourses().get(i).getLabel();
             g2d.fillRect(heightForElement, base, width, end - base);
             g2d.setPaint(Color.black);
-            g2d.drawString(message, 125, holeNumber(timetable.getCourses().get(i).getStart())*heightForElement + 30);
+            g2d.drawString(message, 75, holeNumber(timetable.getCourses().get(i).getStart())*heightForElement + 30);
             message = timetable.getCourses().get(i).getClassroom();
-            g2d.drawString(message, 125, holeNumber(timetable.getCourses().get(i).getStart())*heightForElement + 40 + (g2d.getFontMetrics().getHeight()));
+            g2d.drawString(message, 75, holeNumber(timetable.getCourses().get(i).getStart())*heightForElement + 40 + (g2d.getFontMetrics().getHeight()));
+            message = timetable.getCourses().get(i).getStart() + " - " + timetable.getCourses().get(i).getEnd();
+            g2d.drawString(message, 75, holeNumber(timetable.getCourses().get(i).getStart())*heightForElement + 65 + (g2d.getFontMetrics().getHeight()));
         }
         return g2d;
     }
